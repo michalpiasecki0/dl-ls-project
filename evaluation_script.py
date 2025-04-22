b361b073b902cbf92a2fb875fd9988507b35b2f6
Misha Piasecki <michal.piasecki0@gmail.com>
2025-04-22 21:20:31 +0200

    first notes

---
+++ evaluation_script.py
@@ -0,0 +1,27 @@
+import sys
+
+import torch
+import torch.nn as nn
+
+
+def load_model(model_path: str) -> nn.Module:
+    try:
+        return torch.load(model_path)
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        sys.exit(1)
+
+def evaluate(model: nn.Module, dataset):
+    pass
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python evaluation_script.py <arg1> <arg2>")
+        sys.exit(1)
+
+    model_path = sys.argv[1]
+    path_test = sys.argv[2]
+
+    model = load_model(model_path)
+    evaluate(model, path_test)
+    
